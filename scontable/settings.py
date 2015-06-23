b085bf121f4872f8cfbe7a435676ca152c78d417
zeropaul <landerspaulzero@gmail.com>
2015-06-23 15:46:12 -0500

    agregando accounts

--- scontable/settings.py
+++ scontable/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #App's
-    #'home',
+    'accounts',
     'clientes',
     'proveedores',
     'productos',
