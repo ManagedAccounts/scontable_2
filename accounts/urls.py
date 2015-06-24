3058faaca1caedb8ebb1f12f8414f6391d04a0c5
zeropaul <landerspaulzero@gmail.com>
2015-06-24 14:58:58 -0500

    gregando login class

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+"""from django.conf.urls import include, url
+from django.contrib import admin
+from accounts.views import ControlPanelView, LoginView, LogoutView
+
+
+urlpatterns = [
+    url(r'^$', ControlPanelView.as_view(), name='inicio' ),
+    url(r'^login/$',LoginView.as_view(), name='login'),
+    url(r'^logout/$',LogoutView.as_view(), name='logout'),
+
+]""""
+
