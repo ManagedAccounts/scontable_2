edaac6d300e82f27cf3e759b7a742e85fa5830a0
zeropaul <landerspaulzero@gmail.com>
2015-06-26 15:02:56 -0500

    lista de compras

--- compras/views.py
+++ compras/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from compras.models import Ordencompra
 
 # Create your views here.
+
+class OrdencompraList(ListView):
+    model = Ordencompra
+
+
