76f1e87fa67800bda477ca6c25407a83385db2d2
zeropaul <landerspaulzero@gmail.com>
2015-06-28 16:55:18 -0500

    agregando n_n

--- scontable/settings.py
+++ scontable/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'd&@zlkgptiskghu+#%e&*67c)x9vi@8%(4fn$-eb_nn7jfims0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
